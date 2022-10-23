012b1774fb910a8133939ea164cb35721715faa6
Jinlei2000 <91153798+Jinlei2000@users.noreply.github.com>
2022-10-23 14:40:36 +0200

    card afgewerkt + animation logo

--- script/app.js
+++ script/app.js
@@ -1,9 +1,22 @@
-
-
-
-const init = function() {
-    console.log('App initialized');
+const Listeners = function () {
+  console.log('Listeners initialized');
+  //listen to the hover event on the button
+  document.querySelector('.js-logo').addEventListener('mouseover', function () {
+    console.log('Logo is mouseover');
+    //animation spin
+    document.querySelector('.c-logo__symbol').style.animation = 'spin 1s infinite linear';
+    //check mouseout
+    document.querySelector('.js-logo').addEventListener('mouseout', function () {
+        console.log('Logo is mouseout');
+        //delete animation
+        document.querySelector('.c-logo__symbol').style.animation = '';
+    });
+  });
 };
 
-document.addEventListener("DOMContentLoaded", init);
+const init = function () {
+  console.log('App initialized');
+  Listeners();
+};
 
+document.addEventListener('DOMContentLoaded', init);
